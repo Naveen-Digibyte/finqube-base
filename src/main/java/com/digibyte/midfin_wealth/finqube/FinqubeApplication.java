faee01a114f7802237c4aadaaf643d6ef52fb06d
Siddarth Natarajan <siddarthnatarajan@Siddarths-iMac.local>
2025-02-08 18:52:47 +0530

    Intitial Commit

---
+++ src/main/java/com/digibyte/midfin_wealth/finqube/FinqubeApplication.java
@@ -0,0 +1,13 @@
+package com.digibyte.midfin_wealth.finqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinqubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinqubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digibyte/midfin_wealth/finqube/FinqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digibyte.midfin_wealth.finqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinqubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
